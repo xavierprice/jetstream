790f7e1977d0ad474fb74167e8e2dfe6480da178
Xavier <xavierpriceau@gmail.com>
2023-11-08 15:33:41 +1000

    init commit - Home.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./styles/app.scss";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./components/Home.jsx";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   );
 };
