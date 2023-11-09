a35ec7f2243b32f73bf0f3ed20ce1f92448927c6
Xavier <xavierpriceau@gmail.com>
2023-11-09 11:05:04 +1000

    add form jsx and scss

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -1,7 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <div></div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitted Data:");
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Phone:", phone);
+    console.log("Address:", address);
+    console.log("Message:", message);
+
+    setName("");
+    setEmail("");
+    setPhone("");
+    setAddress("");
+    setMessage("");
+  };
+
+  return (
+    <div className="home-page">
+      <div className="quote-form-container">
+        <h2>Request a Free Quote!</h2>
+        <form onSubmit={handleSubmit} className="quote-form">
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="phone">Phone Number:</label>
+            <input
+              type="tel"
+              id="phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="address">Street Address</label>
+            <input
+              type="text"
+              id="address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              rows="4"
+              required
+            />
+          </div>
+          <button type="submit" className="submit-button">
+            Request Quote
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
