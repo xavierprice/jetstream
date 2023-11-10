a4ceeeeb3f9466cf9d02c626b98cd618fcfaf6c4
Xavier <xavierpriceau@gmail.com>
2023-11-10 14:48:43 +1000

    add image section for home

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -25,64 +25,70 @@ const Home = () => {
 
   return (
     <div className="home-page">
-      <div className="quote-form-container">
-        <h2>Request a Free Quote!</h2>
-        <form onSubmit={handleSubmit} className="quote-form">
-          <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="phone">Phone Number:</label>
-            <input
-              type="tel"
-              id="phone"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="address">Street Address</label>
-            <input
-              type="text"
-              id="address"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="message">Message:</label>
-            <textarea
-              id="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              rows="4"
-              required
-            />
-          </div>
-          <button type="submit" className="submit-button">
-            Request Quote
-          </button>
-        </form>
-      </div>
+      <section className="left-half-home">
+        <div className="home-images"></div>
+      </section>
+
+      <section className="right-half-home">
+        <div className="quote-form-container">
+          <h2>Request a Free Quote!</h2>
+          <form onSubmit={handleSubmit} className="quote-form">
+            <div className="form-group">
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Phone Number:</label>
+              <input
+                type="tel"
+                id="phone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="address">Street Address</label>
+              <input
+                type="text"
+                id="address"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Message:</label>
+              <textarea
+                id="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                rows="4"
+                required
+              />
+            </div>
+            <button type="submit" className="submit-button">
+              Request Quote
+            </button>
+          </form>
+        </div>
+      </section>
     </div>
   );
 };
