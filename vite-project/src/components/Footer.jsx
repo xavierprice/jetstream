8ef344fa29da8e9e6ba5c4f02e08eb114b68680a
Xavier <xavierpriceau@gmail.com>
2023-11-15 14:55:29 +1000

    make footer a component displayed on all pages

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -3,18 +3,16 @@ import { FaCopyright, FaInstagram, FaFacebookSquare } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <section className="footer-container">
-      <footer>
-        <div className="footer-copyright">
-          <FaCopyright />
-          2023 JETSTREAM PRESSURE WASHING
-        </div>
-        <div className="footer-socials">
-          <FaInstagram className="fa-icon" />
-          <FaFacebookSquare className="fa-icon" />
-        </div>
-      </footer>
-    </section>
+    <footer>
+      <div className="footer-copyright">
+        <FaCopyright />
+        2023 JETSTREAM PRESSURE WASHING
+      </div>
+      <div className="footer-socials">
+        <FaInstagram className="fa-icon" />
+        <FaFacebookSquare className="fa-icon" />
+      </div>
+    </footer>
   );
 };
 
