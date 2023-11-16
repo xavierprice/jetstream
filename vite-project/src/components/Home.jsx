2a15ef47f1f225d34c90b5b71fabd38737f18929
Xavier <xavierpriceau@gmail.com>
2023-11-16 13:06:32 +1000

    add main heading to home page

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -2,9 +2,20 @@ import React, { useState } from "react";
 import { FaHouseUser, FaArrowDown } from "react-icons/fa";
 
 const Home = () => {
-
   return (
     <div className="home-page">
+      <section className="main-heading">
+        <h2>
+          Revitalize Your Property's Sparkle with Professional Pressure Washing
+          Services!
+        </h2>
+        <p>
+          Enhance your property's curb appeal and cleanliness effortlessly with
+          our top-tier pressure washing services. From driveways to exteriors,
+          we restore the shine to your surfaces, making your property look brand
+          new.
+        </p>
+      </section>
       <section className="home-services">
         <h2>
           Services we offer <FaArrowDown style={{ marginLeft: "10px" }} />
