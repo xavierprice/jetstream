87b5934eaf6055d50ee030a7562a39751b28f41c
Xavier <xavierpriceau@gmail.com>
2023-11-23 14:28:35 +1000

    edit footer jsx and scss

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -1,16 +1,54 @@
 import React from "react";
-import { FaCopyright, FaInstagram, FaFacebookSquare } from "react-icons/fa";
+import {
+  FaInstagram,
+  FaFacebookSquare,
+  FaPhone,
+  FaEnvelope,
+  FaMapMarkerAlt,
+  FaCopyright,
+  FaTiktok
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-info">
+          <h3>Contact Information</h3>
+          <ul className="contact-list">
+            <li>
+              <FaMapMarkerAlt />
+              Brisbane, QLD, Australia
+            </li>
+            <li>
+              <FaPhone />
+              0491 016 876
+            </li>
+            <li>
+              <FaEnvelope />
+              jetstreampressurewashingau@gmail.com
+            </li>
+          </ul>
+        </div>
+        <div className="footer-socials">
+          <h3>Follow Us</h3>
+          <div className="social-icons">
+            <a href="#" target="" rel="">
+              <FaFacebookSquare className="fa-icon" />
+            </a>
+            <a href="#" target="" rel="">
+              <FaInstagram className="fa-icon" />
+            </a>
+            <a href="#" target="" rel="">
+              <FaTiktok className="fa-icon" />
+            </a>
+          </div>
+        </div>
+      </div>
       <div className="footer-copyright">
         <FaCopyright />
-        2023 JETSTREAM PRESSURE WASHING
-      </div>
-      <div className="footer-socials">
-        <FaInstagram className="fa-icon" />
-        <FaFacebookSquare className="fa-icon" />
+        {new Date().getFullYear()} JETSTREAM PRESSURE WASHING. All Rights
+        Reserved.
       </div>
     </footer>
   );
