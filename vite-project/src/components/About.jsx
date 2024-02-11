18e6ecbc1cf3a5cf3eab5e4b89ba109612ec2e22
Xavier <xavierpriceau@gmail.com>
2024-02-11 20:41:45 +1000

    add benefits and testimonial sections to about page

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import {
+  FaRegClock,
+  FaHandSparkles,
+  FaTrophy,
+  FaUmbrella,
+} from "react-icons/fa";
 import workImage2 from "../assets/work-image2.jpg";
 import workImage1 from "../assets/work-Image1.jpg";
 import workImage1Cropped from "../assets/work-Image1-heightCropped.png";
@@ -6,21 +12,76 @@ import workImage4 from "../assets/workImage4.jpeg";
 
 const About = () => {
   const workImages = [workImage1, workImage2, workImage2, workImage1Cropped];
+  const benefits = [
+    {
+      icon: <FaRegClock />,
+      title: "Fast",
+      description: "Get quick results and save time with our speedy service.",
+    },
+    {
+      icon: <FaHandSparkles />,
+      title: "Clean",
+      description:
+        "Enjoy a clutter-free experience with our sleek and organized interface.",
+    },
+    {
+      icon: <FaTrophy />,
+      title: "Quality",
+      description:
+        "Experience top-notch performance and reliability with our high-quality products.",
+    },
+    {
+      icon: <FaUmbrella />,
+      title: "Insurance",
+      description:
+        "Stay protected and secure with our comprehensive insurance coverage.",
+    },
+  ];
+  const testimonials = [
+    {
+      id: 1,
+      name: "John Doe",
+      text: '"I was amazed by the quality of service provided by ABC Pressure Washing. My house looks brand new!"',
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      text: '"The team at ABC Pressure Washing did a fantastic job on my driveway. I highly recommend their services!"',
+    },
+    {
+      id: 3,
+      name: "Bob Johnson",
+      text: '"Professional and efficient! ABC Pressure Washing exceeded my expectations. Will hire them again."',
+    },
+  ];
 
   return (
     <main className="about-page">
       <section className="header-section">
         <div className="main-container">
-          <h1>About Us</h1>
-          <p>
-            Welcome to Jetstream Pressure Washing, where we specialize in
-            restoring the beauty of your property with top-quality pressure
-            washing services. Our experienced team uses advanced equipment and
-            eco-friendly solutions to deliver remarkable results, leaving your
-            surfaces looking pristine and renewed. Choose Jetstream for
-            reliable, efficient, and affordable cleaning solutions that enhance
-            your property's curb appeal and longevity.
-          </p>
+          <section className="intro">
+            <h1>About Us</h1>
+            <p>
+              Welcome to Jetstream Pressure Washing, where we specialize in
+              restoring the beauty of your property with top-quality pressure
+              washing services. Our experienced team uses advanced equipment and
+              eco-friendly solutions to deliver remarkable results, leaving your
+              surfaces looking pristine and renewed. Choose Jetstream for
+              reliable, efficient, and affordable cleaning solutions that
+              enhance your property's curb appeal and longevity.
+            </p>
+          </section>
+          <section className="benefits-section">
+            <div className="benefit-container">
+              {benefits.map((benefit) => (
+                <div className="benefit" key={benefit.title}>
+                  <div className="benefit-icon">{benefit.icon}</div>
+                  <h2>{benefit.title}</h2>
+                  <p>{benefit.description}</p>
+                </div>
+              ))}
+            </div>
+          </section>
         </div>
       </section>
       <section className="vision-section">
@@ -73,6 +134,19 @@ const About = () => {
               enhance properties and positively impact communities. Join us in
               elevating standards in pressure washing.
             </p>
+          </div>
+        </div>
+      </section>
+      <section className="testimonial-section">
+        <div className="main-container">
+          <h1>Great things our client say</h1>
+          <div className="testimonial-list">
+            {testimonials.map((testimonial) => (
+              <div key={testimonial.id} className="testimonial-item">
+                <p className="testimonial-text">{testimonial.text}</p>
+                <p className="testimonial-author">- {testimonial.name}</p>
+              </div>
+            ))}
           </div>
         </div>
       </section>
