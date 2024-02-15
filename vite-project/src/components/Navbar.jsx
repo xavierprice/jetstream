20fa6184b906f16eed52b22c4f12c6b34444b86f
Xavier <xavierpriceau@gmail.com>
2024-02-15 14:30:43 +1000

    navbar touch up and mobile menu layout

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Logo from "../assets/JPW.png";
-import { FaBars, FaTimes, FaPhoneAlt } from "react-icons/fa";
+import { FaBars, FaTimes, FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
@@ -29,12 +29,10 @@ const Navbar = () => {
   return (
     <div className="navbar-component">
       <li className="navbar-left">
-        <Link to="/">
-          <h1>JPW</h1>
+        <Link to="/" onClick={closeMenu}>
+          <h1 className={!nav ? "" : "navbar-left alt"}>JPW</h1>
         </Link>
       </li>
-      {/* menu */}
-
       <ul className="navbar-headings">
         <li className={location.pathname === "/" ? "active" : ""}>
           <Link to="/">Home</Link>
@@ -48,42 +46,37 @@ const Navbar = () => {
         <li className={location.pathname === "/contact" ? "active" : ""}>
           <Link to="/contact">Contact</Link>
         </li>
-        <li className={location.pathname === "/quote" ? "active" : ""}>
-          <Link to="/quote">Quote</Link>
-        </li>
       </ul>
-
       <ul className="navbar-contact">
+        {/* <li className={!nav ? "hidden" : "contact-bubble"}>
+          <FaEnvelope style={{ fontSize: "20px", paddingRight: "0.4rem" }} />
+          Free quote
+        </li> */}
         <li className="contact-bubble phone-bubble">
           <FaPhoneAlt style={{ fontSize: "20px", paddingRight: "0.4rem" }} />
           0491 016 876
         </li>
       </ul>
-
-      {/* hamburger menu */}
-
       <li onClick={handleClick} className="hamburger-menu">
-        {!nav ? <FaBars /> : <FaTimes />}
+        {!nav ? <FaBars /> : <FaTimes className="faTimes" />}
       </li>
-
-      {/* Mobile menu */}
       <ul className={!nav ? "hidden" : "mobile-menu"}>
-        <li>
+        <li className={location.pathname === "/" ? "active" : ""}>
           <Link to="/" onClick={closeMenu}>
             Home
           </Link>
         </li>
-        <li>
+        <li className={location.pathname === "/services" ? "active" : ""}>
           <Link to="/services" onClick={closeMenu}>
             Services
           </Link>
         </li>
-        <li>
+        <li className={location.pathname === "/about" ? "active" : ""}>
           <Link to="/about" onClick={closeMenu}>
             About
           </Link>
         </li>
-        <li>
+        <li className={location.pathname === "/contact" ? "active" : ""}>
           <Link to="/contact" onClick={closeMenu}>
             Contact
           </Link>
