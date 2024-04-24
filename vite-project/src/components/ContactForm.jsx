b4c08b068655dde22af483b21b8885f726af90ed
Xavier <xavierpriceau@gmail.com>
2024-04-24 14:06:01 +1000

    add submitted message to contact form

--- vite-project/src/components/ContactForm.jsx
+++ vite-project/src/components/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 const ContactForm = ({ services, selectedService, setSelectedService }) => {
@@ -7,6 +7,10 @@ const ContactForm = ({ services, selectedService, setSelectedService }) => {
   const [phone, setPhone] = useState("");
   const [area, setArea] = useState("");
   const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const formContainerRef = useRef(null);
+  const [submissionContainerDimensions, setSubmissionContainerDimensions] =
+    useState({ height: 0 });
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,8 +23,10 @@ const ContactForm = ({ services, selectedService, setSelectedService }) => {
     setName("");
     setEmail("");
     setPhone("");
-    setAddress("");
+    setSelectedService("");
+    setArea("");
     setMessage("");
+    setSubmitted(true);
   };
 
   const navigate = useNavigate();
@@ -31,100 +37,118 @@ const ContactForm = ({ services, selectedService, setSelectedService }) => {
     const selectedService = services[selectedId];
 
     setSelectedService(selectedService ? selectedService.title : "");
+
+    if (formContainerRef.current) {
+      const { height } = formContainerRef.current.getBoundingClientRect();
+      setSubmissionContainerDimensions({ height });
+    }
   }, [id]);
+
+  const submissionContainerStyle = {
+    width: "100%",
+    height: submissionContainerDimensions.height,
+  };
 
   return (
     <div className="contact-component">
       <div className={`quote-form-container`}>
-        <h1>Claim your free estimate</h1>
-        <form onSubmit={handleSubmit} className="quote-form">
-          <div className="form-group double-label">
-            <span>
-              <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                id="name"
-                placeholder="John Smith"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
-            </span>
-            <span>
-              <label htmlFor="phone">Phone:</label>
-              <input
-                type="tel"
-                id="phone"
-                placeholder="0411 222 333"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-                required
-              />
-            </span>
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              placeholder="johnsmith@example.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group double-label">
-            <span>
-              <label htmlFor="service">Service Needed:</label>
-              <select
-                id="service"
-                value={selectedService || ""}
-                onChange={(e) => setSelectedService(e.target.value)}
-                required
-              >
-                <option value="" disabled hidden>
-                  Select Service
-                </option>
-                {services.map((service, index) => (
-                  <option key={index} value={service.title}>
-                    {service.title}
-                  </option>
-                ))}
-              </select>
-            </span>
+        {!submitted ? (
+          <div ref={formContainerRef}>
+            <h1>Claim your free estimate</h1>
+            <form onSubmit={handleSubmit} className="quote-form">
+              <div className="form-group double-label">
+                <span>
+                  <label htmlFor="name">Name:</label>
+                  <input
+                    type="text"
+                    id="name"
+                    placeholder="John Smith"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                </span>
+                <span>
+                  <label htmlFor="phone">Phone:</label>
+                  <input
+                    type="tel"
+                    id="phone"
+                    placeholder="0411 222 333"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    required
+                  />
+                </span>
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="johnsmith@example.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-group double-label">
+                <span>
+                  <label htmlFor="service">Service Needed:</label>
+                  <select
+                    id="service"
+                    value={selectedService || ""}
+                    onChange={(e) => setSelectedService(e.target.value)}
+                    required
+                  >
+                    <option value="" disabled hidden>
+                      Select Service
+                    </option>
+                    {services.map((service, index) => (
+                      <option key={index} value={service.title}>
+                        {service.title}
+                      </option>
+                    ))}
+                  </select>
+                </span>
 
-            <span>
-              <label htmlFor="area">Area Size:</label>
-              <select
-                id="area"
-                value={area}
-                onChange={(e) => setArea(e.target.value)}
-                required
-              >
-                <option value="" disabled hidden>
-                  Select Area Size
-                </option>
-                <option value="Small">0 - 50sqm </option>
-                <option value="Medium">100 - 200sqm</option>
-                <option value="Large">200 - 400sqm</option>
-              </select>
-            </span>
+                <span>
+                  <label htmlFor="area">Area Size:</label>
+                  <select
+                    id="area"
+                    value={area}
+                    onChange={(e) => setArea(e.target.value)}
+                    required
+                  >
+                    <option value="" disabled hidden>
+                      Select Area Size
+                    </option>
+                    <option value="Small">0 - 50sqm </option>
+                    <option value="Medium">100 - 200sqm</option>
+                    <option value="Large">200 - 400sqm</option>
+                  </select>
+                </span>
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message:</label>
+                <textarea
+                  id="message"
+                  value={message}
+                  placeholder="Message..."
+                  onChange={(e) => setMessage(e.target.value)}
+                  rows="4"
+                  required
+                />
+              </div>
+              <button type="submit" className="submit-button">
+                Submit request
+              </button>
+            </form>
           </div>
-          <div className="form-group">
-            <label htmlFor="message">Message:</label>
-            <textarea
-              id="message"
-              value={message}
-              placeholder="Message..."
-              onChange={(e) => setMessage(e.target.value)}
-              rows="4"
-              required
-            />
+        ) : (
+          <div className="submission-message" style={submissionContainerStyle}>
+            <h2>{`Thank you! Your message has been sent. (for demo purposes only)`}</h2>
           </div>
-          <button type="submit" className="submit-button">
-            Submit request
-          </button>
-        </form>
+        )}
       </div>
     </div>
   );
